e4e73378d28f9b52ae8b343408a4c0791452cb35
mishraayushgit <156198404+mishraayushgit@users.noreply.github.com>
2026-02-22 22:45:07 +0530

    Update cnn_model.py

--- Model/cnn_model.py
+++ Model/cnn_model.py
@@ -6,27 +6,19 @@ class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
 
-        # Convolutional layers
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
-
-        # Fully connected layers
         self.fc1 = nn.Linear(64 * 5 * 5, 128)
         self.fc2 = nn.Linear(128, 10)
 
-    def forward(self, x):
-        # Convolution + ReLU + Pooling
+def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2)
-
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
-
-        # Flatten
         x = x.view(x.size(0), -1)
-
-        # Fully connected layers
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
 
-        return x
+ return x
+
